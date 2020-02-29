bb2d3a178debf9ebfcb05c4b668ef8ae05033e0a
zhoushibo <1216147314@qq.com>
2020-02-29 14:56:23 +0800

    elementui配置

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册element-ui 任何位置都可以使用
 new Vue({
   router,
   render: h => h(App)
