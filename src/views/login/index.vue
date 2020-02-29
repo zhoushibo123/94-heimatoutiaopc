62f10d3b2d02c89fb408301abf71786802471690
zhoushibo <1216147314@qq.com>
2020-02-29 20:01:21 +0800

    登录页面输入框

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,44 @@
 <template>
-  <div>
-      <h1>我是登陆界面</h1>
+ <div style="position:relative">
+     <!-- 头像 -->
+     <div class="demo-type">
+      <el-avatar class="zh" src="https://wx3.sinaimg.cn/mw690/006NcDUhly1gcdiklv0tbj30n00tfdgk.jpg"></el-avatar>
+     </div>
+      <div class="login">
+      <!-- <span>我是登陆界面</span> -->
+      <!-- 表单内容 -->
+      <!-- logo -->
+      <el-card class="login-card">
+          <div class="title">
+              <img src="../../assets/img/cxdlz.png" alt="">
+          </div>
+          <!-- 表单 -->
+          <el-form style="margin-top:20px">
+              <!-- 表单容器 -->
+              <el-form-item>
+              <!--表单域  -->
+              <el-input placeholder="请输入手机号">
+              </el-input>
+              </el-form-item>
+               <!-- 验证码 -->
+            <el-form-item>
+                <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+                 <!-- 放置一个按钮 -->
+                <el-button style="float:right" plain>发送验证码</el-button>
+            </el-form-item>
+            <!-- 表单域 -->
+            <el-form-item>
+           <!-- 是否同意被人家坑 -->
+           <el-checkbox>我已阅读同意用户协议和隐私条款</el-checkbox>
+           </el-form-item>
+           <!-- 按钮 -->
+         <el-form-item>
+           <el-button style="width:100%" type="primary">登录</el-button>
+         </el-form-item>
+          </el-form>
+      </el-card>
   </div>
+ </div>
 </template>
 
 <script>
@@ -10,6 +47,34 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+// 如果需要写less  在style上lang="less" scoped只对自己的样式起作用
+.login{
+    background-image: url('../../assets/img/bj.jpg');
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card{
+        width: 440px;
+       height: 380px;
+         filter:alpha(opacity=60);
+            /* CSS3 standard */
+        opacity:0.8;
+        .title{
+            text-align: center;
+            img{
+                height: 100px;
+            }
+        }
+    }
+}
+.demo-type{
+position: absolute;
+margin: 10px 0;
+.zh{
+background-size:cover;
+}
+}
 </style>
