6ad4407a8f756a6e0058ecc64e02001e8a1f66e3
zhoushibo <1216147314@qq.com>
2020-02-29 15:42:21 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import '@/styles/index.less'
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册element-ui 任何位置都可以使用
 new Vue({
